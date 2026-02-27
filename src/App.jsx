dededaf1193ff6789c696466532fd7a0a1027c0d
GuifreCom <gcompanyo@xarxa.fedac.cat>
2026-02-27 18:56:10 +0100

    2nd commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,12 @@
 import './App.css'
-import IncrementDecrement from './components/tasca1/Exercici1'
-import Cercle from './components/tasca1/Exercici2'
-import Lorem from './components/tasca1/Exercici3'
-import CanviImatge from './components/tasca1/Exercici4'
+import Menu from './menu'
 
 function App() {
 
 
   return (
     <>
-    <IncrementDecrement/>
-    <Cercle/>
-    <Lorem/>
-    <CanviImatge/>
+    <Menu/>
     </>
   )
 }
---
+++ src/menu.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react"
+import IncrementDecrement from './components/tasca1/Exercici1'
+import Cercle from './components/tasca1/Exercici2'
+import Lorem from './components/tasca1/Exercici3'
+import CanviImatge from './components/tasca1/Exercici4'
+function Menu() {
+    const [tasca, setTasca] = useState(0)
+    if (tasca === 1) {
+        return (
+            <>
+            <IncrementDecrement/>
+            <Cercle/>
+            <Lorem/>
+            <CanviImatge/>
+            </>
+        )
+    }
+    const tasca1 = () => {
+        setTasca(1)
+    }
+
+  return (
+    <div >
+        Menu Intereactiu!
+        <div >
+            <button onClick={tasca1}> Tasca 1</button>
+        </div>
+    </div>
+  )
+}
+
+export default Menu
