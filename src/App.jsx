f2c648e6423849781360e5a224d4d5b9caf21d0b
GuifreCom <gcompanyo@xarxa.fedac.cat>
2026-03-16 20:47:11 +0100

    putamerda

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,38 @@
+import { useState } from 'react'
 import './App.css'
+import IncrementDecrement from './components/tasca1/Exercici1'
+import Cercle from './components/tasca1/Exercici2'
+import Lorem from './components/tasca1/Exercici3'
+import CanviImatge from './components/tasca1/Exercici4'
+import Galeria from './components/tasca2/Exercici1'
+import Carousel from './components/tasca2/Exercici2'
 import Menu from './menu'
 
 function App() {
 
+  const [tascaSeleccionada, setTascaSeleccionada] = useState("tasca1.1")
+
+  function getTascaComponent(){
+    switch (tascaSeleccionada) {
+      case "tasca1.1":
+        return <IncrementDecrement/>
+      case "tasca1.2":
+        return <Cercle/>
+      case  "tasca1.3":
+        return <Lorem/>
+      case "tasca1.4":
+        return <CanviImatge/>
+      case "tasca2.1":
+        return <Galeria/>
+      case "tasca2.2":
+        return <Carousel/>
+    }
+  }
 
   return (
     <>
-    <Menu/>
+    <Menu canviarTasca={setTascaSeleccionada}/>
+    {getTascaComponent()}
     </>
   )
 }
--- src/menu.jsx
+++ src/menu.jsx
@@ -1,50 +1,21 @@
-import { useState } from "react"
-import IncrementDecrement from './components/tasca1/Exercici1'
-import Cercle from './components/tasca1/Exercici2'
-import Lorem from './components/tasca1/Exercici3'
-import CanviImatge from './components/tasca1/Exercici4'
-import Galeria from "./components/tasca2/Exercici1"
-import Carousel from "./components/tasca2/Exercici2"
-function Menu() {
-    const [tasca, setTasca] = useState(0)
-    if (tasca === 1) {
-        return (
-            <>
-            <IncrementDecrement/>
-            <Cercle/>
-            <Lorem/>
-            <CanviImatge/>
-            </>
-        )
-    }
-    else if (tasca === 2){
-        return(
-            <>
-            <Galeria/>
-            <Carousel/>
-            </>
-        )
-    }
-    const tasca1 = () => {
-        setTasca(1)
-    }
-    const tasca2 = () => {
-        setTasca(2)
-    }
 
-  return (
-    <div >
-        <h1>Menu Reactiu Briar!!!</h1>
+function Menu({canviarTasca}) {
+    const menuItems = ["tasca1.1", "tasca1.2", "tasca1.3", "tasca1.4", "tasca2.1", "tasca2.2"]
+    function onClickMenuItem(item){
+        canviarTasca(item)
+    }
+    return (
         <div style={{
-            display:"flex",
-            flexDirection:"column",
-            gap:"5px"
+            position:"fixed",
+            top:"35px",
+            left: 0,
+            width: '100vw'
+            
         }}>
-            <button onClick={tasca1}> Tasca 1</button>
-            <button onClick={tasca2}> Tasca 2</button>
+            {menuItems.map((item) => {
+                return <button key={item} onClick={() => onClickMenuItem(item)}>{item}</button>
+            })}
         </div>
-    </div>
-  )
+    )
 }
-
 export default Menu
